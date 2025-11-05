924aa4b62b8db58ce837246e011586223d13ad6e
Hitaishi Lohtia <hitaishi.8.8.8@gmail.com>
2025-11-05 21:12:37 +0530

    Time: 15 ms (5.06%) | Memory: 46.9 MB (5.14%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,12 +1,29 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         if(x < 0) {
+//             return false;
+//         } 
+//         String s = Integer.toString(x) ;
+
+//         String rev = new StringBuffer(s).reverse().toString();
+
+//         return s.equals(rev);
+//     }
+// }
+
+
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0) {
-            return false;
-        } 
-        String s = Integer.toString(x) ;
+        if (x < 0) return false; // negative numbers not palindrome
 
-        String rev = new StringBuffer(s).reverse().toString();
+        String s = Integer.toString(x); // convert to string
+        String rev = ""; // empty string to store reversed value
 
-        return s.equals(rev);
+        // reverse manually
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev = rev + s.charAt(i);
+        }
+
+        return s.equals(rev); // compare both
     }
 }
