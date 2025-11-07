4f1a08578a373e6f2c2800e9aa4ad6553275b748
Hitaishi Lohtia <hitaishi.8.8.8@gmail.com>
2025-11-07 20:59:03 +0530

    Time: 3 ms (61.36%) | Memory: 43.4 MB (6.28%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,10 +2,9 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
 
-
         for(char ch : s.toCharArray())
         {
-            if(ch =='(' || ch == '{' || ch == '[')
+            if(ch == '(' || ch == '{' || ch == '[')
             {
                 st.push(ch);
             }
@@ -13,15 +12,15 @@ class Solution {
             {
                 if(st.isEmpty()) return false ;
 
-
                 char top = st.pop();
 
-                if((ch == ')' && top != '('  ) ||
-                    (ch == '}' && top != '{')  ||
-                    (ch == ']' && top != '[')
-                )  {
-                    return false ;
-                }
+                if((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '['))
+
+                    {
+                        return false ;
+                    }
             }
         }
         return st.isEmpty();
