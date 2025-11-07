91161daba8815c2aa53d682d19c4573365308e7c
Hitaishi Lohtia <hitaishi.8.8.8@gmail.com>
2025-11-07 20:23:31 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (74.89%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length-1;
+        int left = 0 ;
+        int right = s.length-1 ;
 
         while(left < right)
         {
@@ -9,8 +9,8 @@ class Solution {
             s[left] = s[right];
             s[right] = temp;
 
-            left ++;
-            right -- ;
+            left ++ ;
+            right -- ; 
         }
     }
 }
